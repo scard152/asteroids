2eea0665295b868898fe52f0a7a4dd35d8507b24
scard152 <scard152@gmail.com>
2025-09-08 17:01:38 -0700

    sprites start

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    
 
     while True:
         for event in pygame.event.get():
@@ -13,6 +16,8 @@ def main():
         screen.fill((0,0,0))
 
         pygame.display.flip()
+
+        dt = clock.tick(60)/1000
 
 
 
